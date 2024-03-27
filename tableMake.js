a21ec462db111db120aada7bf3c9c5360c434c4f
kklugewicz <161895041+kklugewicz@users.noreply.github.com>
2024-03-27 00:23:18 -0400

    Update tableMake.js

--- tableMake.js
+++ tableMake.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var input = document.getElementById("ticker").value;
         console.log("Ticker input value:", input);
         var data = {'ticker': input};
-        fetch('http://127.0.0.1:5000/main', {
+        fetch('https://pythia-14fbe9516611.herokuapp.com/main', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -206,7 +206,7 @@ function openBox(header) {
 function fetchData2(inputValue,data1){
     console.log("Ticker input value:", inputValue);
     var data = {'ticker': inputValue};
-    fetch('http://127.0.0.1:5000/main', {
+    fetch('https://pythia-14fbe9516611.herokuapp.com/main', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
